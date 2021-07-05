0e8ac539caedc3184278169c683d3af61b591ae4
gkodua58 <kodua22g@mtholyoke.edu>
2021-07-05 17:43:22 -0400

    navbar update

---
+++ po-frontend/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import '../App.css';
+import Navbar from '../jscomponents/Navbar';
+import MainPage from '../jscomponents/MainPage';
+
+export default class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        <MainPage />
+      </div>
+    );
+  }
+}
